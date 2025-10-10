13a54579dc0148d42c949213669df647779c56b9
leyla-kazymova <leyla.anarkyzy2005@gmail.com>
2025-10-11 02:04:08 +1000

    nachalo

--- todo_list/todo_list/todo_list.cpp
+++ todo_list/todo_list/todo_list.cpp
@@ -1,9 +1,14 @@
-﻿#include <iostream>
+﻿// file: src/todo_console.cpp
+#include <iostream>
 #include <vector>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
-int main() {
-    vector<string> tasks; // список задач
-    int choice;
+static string trim(const string& s) { 
+    size_t b = 0, e = s.size();
+    while (b < e && isspace(static_cast<unsigned char>(s[b]))) ++b;
+    while (e > b && isspace(static_cast<unsigned char>(s[e - 1]))) --e;
+    return s.substr(b, e - b);
+}
